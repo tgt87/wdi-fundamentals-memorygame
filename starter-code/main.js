fbe65c87beb4595b73a9007cde39d68ee6bc911e
GT Tan <GT Tan>
2017-01-09 23:14:56 +0800

    Create cards using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,20 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardTwo === cardFour){
+/*if (cardTwo === cardFour){
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
-}
+}*/
+
+var gameBoard = document.getElementById('game-board');
+createCards = function() {
+	for(var i = 0; i < 4; i++){
+		var gameCards = document.createElement('div');
+		gameCards.className = 'card';
+		gameBoard.appendChild(gameCards);
+	};
+};
+
+createCards();
+
